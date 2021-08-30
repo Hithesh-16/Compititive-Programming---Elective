8f10d310f91a1153d1d6cf44d8a9f802fc22fb36
hitesh <kethipelli.hitheshreddy@msitprogram.net>
2021-08-30 09:50:16 +0530

    Done

--- 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
+++ 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
@@ -14,5 +14,21 @@
 # Each row and each column add to 3, but one diagonal adds to 2 and the other to 4.
 
 def ismostlymagicsquare(a):
-	# Your code goes here
-	pass
+	leftD=0
+	rightD=0
+	n=len(a)
+	for i in range(n):
+		leftD +=a[i][i]
+		rightD+=a[i][n-i-1]
+	if leftD != rightD:
+		return False
+	for i in range(n):
+		row=0
+		col=0
+	for j in range(n):
+		row+=a[i][i]		
+		col+=a[j][i]
+	
+	if row!= leftD and col!=leftD:
+		return False
+	return True
